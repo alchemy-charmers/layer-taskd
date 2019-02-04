c50918e5784890b7892e26345626eefd670089ae
Chris Sanders <sanders.chris@gmail.com>
2019-02-04 13:35:53 -0600

    appease the lint gods

--- src/tests/unit/test_actions.py
+++
@@ -1,3 +0,0 @@
-import imp
-import mock
-
--- src/tests/unit/test_lib.py
+++ src/tests/unit/test_lib.py
@@ -4,6 +4,7 @@
 def test_pytest():
     assert True
 
+
 def test_taskd(taskd):
     ''' See if the helper fixture works to load charm configs '''
     assert isinstance(taskd.charm_config, dict)
