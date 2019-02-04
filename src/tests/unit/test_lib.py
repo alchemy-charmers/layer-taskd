a73aa6fe16fbe591e953c03a77582435ceda78b3
Chris Sanders <sanders.chris@gmail.com>
2019-02-04 12:43:00 -0600

    Remove action unit tests, covered in functional testing now instead

--- src/tests/unit/test_lib.py
+++ src/tests/unit/test_lib.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
 
-class TestLib():
-    def test_pytest(self):
-        assert True
+def test_pytest():
+    assert True
 
-    def test_taskd(self, taskd):
-        ''' See if the helper fixture works to load charm configs '''
-        assert isinstance(taskd.charm_config, dict)
+def test_taskd(taskd):
+    ''' See if the helper fixture works to load charm configs '''
+    assert isinstance(taskd.charm_config, dict)
 
-    # Include tests for functions in lib_taskd
+# Include tests for functions in lib_taskd
